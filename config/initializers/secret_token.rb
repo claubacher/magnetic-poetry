3b807809ef0a3be3cece746048a4a13cfb55849d
Chris <claubacher@gmail.com>
2013-11-18 15:23:41 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MagneticPoetry::Application.config.secret_key_base = '7450f9d53da65a14156e68ebfad1d17807feb34e5c2065d321fc0c36f49a58d6048493b5001f99b4f460639b8aa6e8fbbf925493258f229d643ade2260b1d789'
