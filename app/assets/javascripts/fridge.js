6a8e80a62ca986aa04bb5bfb36a8d4b8547d4f30
Chris <claubacher@gmail.com>
2013-11-19 00:59:19 -0800

    Save fridge via ajax

--- app/assets/javascripts/fridge.js
+++ app/assets/javascripts/fridge.js
@@ -1,17 +1,26 @@
 $(function(){
   var poem = [];
 
-  var Magnet = function Magnet(offset) {
+  var Magnet = function Magnet(offset, id) {
+    this.id = id;
     this.top = offset.top;
     this.left = offset.left;
   }
 
   $('.magnet').draggable();
-  $('.fridge').droppable({
+  $('#fridge').droppable({
     accept: '.magnet',
     drop: function(event, ui) {
-      poem.push(new Magnet(ui.offset));
+      poem.push(new Magnet(ui.offset, ui.draggable.attr("id")));
     }
+  });
+
+  $('#save-fridge').on('click', function() {
+    $.ajax({
+      url: '/poems',
+      type: 'POST',
+      data: {poem: JSON.stringify(poem)}
+    });
   });
 });
 
