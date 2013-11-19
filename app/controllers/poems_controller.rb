297e918dc07704e9dc014ebc122e1b533e3c2a60
Chris <claubacher@gmail.com>
2013-11-19 01:06:59 -0800

    poems#create via ajax

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,5 +1,26 @@
 class PoemsController < ApplicationController
   def new
-    @magnets = Magnet.all.each
+    @magnets = Magnet.all
+  end
+
+  def create
+    @poem = Poem.new
+    magnets = JSON.parse(poem_params)
+
+    magnets.each do |m|
+      @poem.inclusions.build(magnet: Magnet.find(m["id"]),
+                             fridge_top: m["top"],
+                             fridge_left: m["left"])
+    end
+
+    if @poem.save
+      redirect_to @poem
+    end
+  end
+
+  private
+
+  def poem_params
+    params.require(:poem)
   end
 end
