47c62500c0185f5db8b3840f99005c5c30a28fde
Chris <claubacher@gmail.com>
2013-11-19 01:11:58 -0800

    View and test for drag n drop n save

--- spec/requests/main_page_spec.rb
+++ spec/requests/main_page_spec.rb
@@ -1,14 +1,27 @@
 require 'spec_helper'
 
 describe 'Main page' do
-  before do
-    @magnet1 = Magnet.create(content: 'foo')
-    @magnet2 = Magnet.create(content: 'bar')
+  before(:all) do
+    @magnets = []
+    @magnets << Magnet.create(content: 'lorem')
+    @magnets << Magnet.create(content: 'ipsum')
+  end
+
+  after(:all) do
+    Magnet.delete_all
   end
 
   it "should list all magnets" do
-    visit '/'
-    expect(page).to have_content(@magnet1.content)
-    expect(page).to have_content(@magnet2.content)
+    visit root_path
+    expect(page).to have_selector(".magnet", text: 'lorem')
+    expect(page).to have_selector(".magnet", text: 'ipsum')
+  end
+
+  it "should save new poems", :js => true do
+    visit root_path
+    magnet = page.first(".magnet")
+    target = page.find('#fridge')
+    magnet.drag_to(target)
+    expect {click_button('Save')}.to change {Poem.count}.by(1)
   end
 end
